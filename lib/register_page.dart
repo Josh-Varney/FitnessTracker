94659b88edd5b94d39f9efb2439c30eb738774b7
Joshua Varney <up2138017@myport.ac.uk>
2023-11-05 22:18:26 +0000

    first commit

---
+++ lib/register_page.dart
@@ -0,0 +1,201 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_log/button_forget.dart';
+import 'package:flutter_log/login_tile.dart';
+import 'package:flutter_log/logo_tile.dart';
+import 'package:flutter_log/text_fields.dart';
+
+class RegisterPage extends StatefulWidget {
+  final Function()? onTap;
+  const RegisterPage({super.key, required this.onTap});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPage();
+}
+
+class _RegisterPage extends State<RegisterPage> {
+  // Text Editing Controller
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  final confirmPasswordController = TextEditingController();
+  // Sign User Up
+  void signUserUp() async {
+    // Loading Circle
+    showDialog(
+      context: this.context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+
+    void showErrorMessage(String message) {
+      showDialog(
+          context: this.context,
+          builder: (context) {
+            return const AlertDialog(
+              title: Text("Incorrect Email Address"),
+            );
+          });
+    }
+
+    try {
+      if (passwordController.text == confirmPasswordController.text) {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text,
+          password: passwordController.text,
+        );
+      } else {
+        showErrorMessage("Passwords do not match");
+      }
+
+      Navigator.pop(this.context);
+    } on FirebaseAuthException catch (e) {
+      // Pop Context
+      Navigator.pop(this.context);
+      // Wrong Username
+      showErrorMessage(e.code);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, //Aligned for iOS phones
+              children: [
+                const SizedBox(height: 25),
+                // Logo Textfield
+                const LogoTiling(
+                    imagePath: 'lib/fitnessImage/fitnessLogo.jpeg'),
+
+                const SizedBox(height: 25),
+
+                Text(
+                  'Let\'s create your account!',
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 16,
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                // Username TextField
+                MyTextField(
+                  control: emailController,
+                  hintText: 'Username',
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 10),
+
+                // Password TextField
+                MyTextField(
+                  control: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 10),
+
+                MyTextField(
+                  control: confirmPasswordController,
+                  hintText: 'Confirm Password',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 25),
+
+                // Sign in Buttons
+                ButtonForget(
+                  onTap: signUserUp,
+                  text: 'Sign Up',
+                ),
+
+                const SizedBox(height: 50),
+
+                //Google + Apple Sign In Prompt
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or continue with',
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+                // Google + Apple sign in buttons
+
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Google Button
+                    LogTile(imagePath: 'lib/fitnessImage/GoogleLogo.png'),
+
+                    SizedBox(width: 25),
+
+                    //Apple Button
+                    LogTile(imagePath: 'lib/fitnessImage/AppleLogo.png')
+                  ],
+                ),
+
+                const SizedBox(height: 60),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        'Login Now',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
